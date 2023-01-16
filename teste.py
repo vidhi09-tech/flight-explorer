57a18f5b94b49ddf0635f79f6b9f6be2de9709d9
Rafael Belokurows <rafabelokurows@gmail.com>
2023-01-16 19:41:08 +0000

    Update teste.py

--- teste.py
+++ teste.py
@@ -1,69 +1,89 @@
-import pandas as pd
-
 from selenium import webdriver
-from webdriver_manager.firefox import GeckoDriverManager
+from webdriver_manager.chrome import ChromeDriverManager
+from bs4 import BeautifulSoup
+import pandas as pd
+import time
+import re
 
-browser = webdriver.Firefox(executable_path=GeckoDriverManager().install())
+desired_width = 320
+pd.set_option('display.width', desired_width)
+pd.set_option('display.max_columns', 8)
 
-from selenium.webdriver.firefox.options import Options
+depart = 'OPO'
+destinations = ['LIS', 'MAD']
+dates = ['2023-01-20', '2023-01-21']
 
+final_df = pd.DataFrame({'depart_from': [],
+  'arrive_at': [],
+  'date': [],
+  'depart_time': [],
+  'arrival_time': [],
+  'price': [],
+  'airline': [],
+  'flight_duration': []})
+  
 options = Options()
 options.binary_location = r'C:\Program Files\Mozilla Firefox\firefox.exe'
-driver = webdriver.Firefox(executable_path=r'C:\Users\rafae\OneDrive\Documentos\.wdm\drivers\geckodriver\win64\v0.32.0\geckodriver.exe', options=options)
-driver.get('http://google.com/')
+#driver = webdriver.Firefox(executable_path=r'C:\Users\rafae\OneDrive\Documentos\.wdm\drivers\geckodriver\win64\v0.32.0\geckodriver.exe', options=options)
 
-import time
-url = "https://www.kayak.com/flights"
-driver.get(url)
-time.sleep(1)
-from selenium.common.exceptions import NoSuchElementException
+for destination in destinations:
 
-# Close cookies pop-up
-accept_cookies_xpath = '/html/body/div[12]/div/div[3]/div/div/div/div/div[1]/div/div[2]/div[2]/div[1]/button/span'
-accept_cookies_xpath =  '/html/body/div/div[1]/div/div[1]/div[2]/svg/path[1]'
-try:
-   driver.find_element_by_xpath(accept_cookies_xpath).click()
-except NoSuchElementException:
-   pass
- 
- 
-departure = 'OPO' # Paris
-arrival = 'LIS' # Tunis
-departure_date = '2023-01-15' # Under the format 'YYYY-MM-DD'
-arrival_date = '2023-01-15'
-flexibility_option = "flexible" #for +/-3 days
+    time.sleep(10)
 
-url = f"https://www.kayak.com/flights/{departure}-{arrival}/{departure_date}-{flexibility_option}/{arrival_date}-{flexibility_option}"
-driver.get(url)
-xpath = '//tag_name[contains(@id, ‘static_text’)]'
-xpath = '//*[contains(@id, "destinaton-airport-display-inner")]'
-from selenium.webdriver.common.keys import Keys
-# From
-from_click_xpath = '/html/body/div[1]/div[1]/main/div[1]/div[1]/div/div[1]/div/div/section[2]/div/div/div/div/div/div[1]/div[2]/div/div[1]/div/div/input'
-from_text_xpath = '/html/body/div[1]/div[1]/main/div[1]/div[1]/div/div[1]/div/div/section[2]/div/div/div/div/div/div[1]/div[2]/div/div[1]/div/div/input'
+    for date in dates:
 
+        url = f'https://www.kayak.com/flights/{depart}-{destination}/{date}?sort=price_a'
 
-departure = 'Porto'
-driver.find_element_by_xpath(from_click_xpath).click()
-driver.find_element_by_xpath(from_text_xpath).send_keys(Keys.BACKSPACE + Keys.BACKSPACE + departure)
-time.sleep(0.5)
-driver.find_element_by_xpath(from_text_xpath).send_keys(Keys.RETURN)
-time.sleep(1)
+        driver = webdriver.Firefox(executable_path=r'C:\Users\rafae\OneDrive\Documentos\.wdm\drivers\geckodriver\win64\v0.32.0\geckodriver.exe', options=options)
+        driver.implicitly_wait(20)
+        driver.get(url)
 
+        time.sleep(20)
 
-to_click_xpath = '/html/body/div[1]/div[1]/main/div[1]/div[1]/div/div[1]/div/div/section[2]/div/div/div/div/div/div[1]/div[2]/div/div[3]/div/div'
-to_text_xpath = '/html/body/div[1]/div[1]/main/div[1]/div[1]/div/div[1]/div/div/section[2]/div/div/div/div/div/div[1]/div[2]/div/div[1]/div/div/input'
+        soup = BeautifulSoup(html, 'html.parser')
 
-arrival = 'Lisbon'
-driver.find_element_by_xpath(from_click_xpath).click()
-driver.find_element_by_xpath(from_text_xpath).send_keys(Keys.BACKSPACE + Keys.BACKSPACE + arrival)
-time.sleep(0.5)
-driver.find_element_by_xpath(from_text_xpath).send_keys(Keys.RETURN)
-time.sleep(1)
+        # departure times
+        d_times_lst = []
+        d_times = soup.findAll('span', attrs={'class': 'depart-time base-time'})
+        for tm1 in d_times:
+            d_times_lst.append(tm1.text)
 
+        # arrival times
+        a_times_lst = []
+        a_times = soup.findAll('span', attrs={'class': 'arrival-time base-time'})
+        for tm2 in a_times:
+            a_times_lst.append(tm2.text)
 
-#https://medium.com/analytics-vidhya/what-if-selenium-could-do-a-better-job-than-your-travel-agency-5e4e74de08b0
-#https://towardsdatascience.com/how-to-analyze-survey-data-with-python-84eff9cc9568
-#https://www.youtube.com/watch?v=nN0OD6HLDJk
-#https://github.com/kcelebi/Flight_Analysis
-#https://github.com/g-ziyan/STAT_545A_547M_Exploratory_Data_Analysis/blob/2054c6da07023e132713e34152921f8a408bac67/class%20exercise/cm112/cm112.Rmd
+        # prices
+        price_lst = []
+        price_tag = re.compile('Common-Booking-MultiBookProvider(.*) Theme-featured-large(.*) multi-row(.*)')
+        prices = soup.findAll('div', attrs={'class': price_tag})
+        for price in prices:
+            price_lst.append(price.text.split('\n')[4].strip())
+
+        # airlines
+        airline_lst = []
+        airlines = soup.findAll('div', attrs={'class': 'bottom', 'dir': 'ltr'})
+        for airline in airlines:
+            airline_lst.append(airline.text.replace('\n', ''))
+            
+        # durations
+        duration_lst = []
+        durations = soup.findAll('div', attrs={'class': 'section duration allow-multi-modal-icons'})
+        for duration in durations:
+            duration_lst.append(' '.join(duration.text.split(' ')[:2]).replace('\n', ''))
+
+        df = pd.DataFrame({'depart_from': depart,
+                           'arrive_at': destination,
+                           'date': date,
+                           'depart_time': d_times_lst[:15],
+                           'arrival_time': a_times_lst[:15],
+                           'price': price_lst[:15],
+                           'airline': airline_lst[:15],
+                           'flight_duration': duration_lst[:15]})
+
+        final_df = pd.concat([final_df, df], ignore_index=True, sort=False)
+
+        driver.close()
+
+        time.sleep(20)
