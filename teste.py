b6c62b0ed5c6a670bcadca6d9beb892ef806bcc8
Rafael Belokurows <rafabelokurows@gmail.com>
2023-03-16 15:37:02 +0000

    Update teste.py

--- teste.py
+++ teste.py
@@ -23,3 +23,33 @@ limit 10
 teste = pd.read_gbq(credentials=credentials,query=query)
 
 teste.to_csv("teste.csv")
+
+tabela = teste.to_html()
+
+import smtplib
+from email.message import EmailMessage
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+import ssl 
+sender = 'rafabelokurows@gmail.com'
+recipient = 'rafabelokurows@gmail.com'
+#password = input(str('Enter your password: '))
+password = os.environ['APP_PASSWORD'] #change this when pushin to Github
+subject = 'Deals on airline tickets out of '+city
+#body = 'Sent using Python (if it worked)'
+
+textBefore = "<p>Hey, check out this new deals I've found for airline tickets out of "+city+".</p>This is the summary of the last run:\n"
+html = textBefore + tabela
+
+message = MIMEMultipart()
+message['From'] = sender
+message['To'] = recipient
+message['Subject'] = subject
+message.attach(MIMEText(html, 'html'))
+
+context = ssl.create_default_context()
+with smtplib.SMTP_SSL('smtp.gmail.com',465,context=context) as smtp:
+    smtp.login(sender,password)
+    smtp.sendmail(sender,recipient,message.as_string())
+    
+print('Email with deals sent to ',recipient)
